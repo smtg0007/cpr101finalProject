5c653dfcfb74c6e1b36f5a13101f0c470556c498
smtg0007 <shaheersohail.ss@gmail.com>
2023-11-22 19:43:25 -0500

    Commenting V1

--- source/converting.c
+++ source/converting.c
@@ -5,16 +5,16 @@
 //converting MODULE Source 
 void converting() {
 /* Version 1 */
-printf("*** Start of Converting Strings to int Demo ***\n");
-char int_string[80];
+printf("*** Start of Converting Strings to int Demo ***\n"); 
+char int_string[80]; //initializing an array of characters which is 80 bytes long 
 int int_number;
     do {
-            printf("Type the int numeric string (q - to quit):\n");
-            fgets(int_string, BUFFER_SIZE, stdin);
-            int_string[strlen(int_string) - 1] = '\0';
-            int_number = atoi(int_string);
-            printf("Converted number is %d\n", int_number);
-        }while(strcmp(int_string, "q"));
+            printf("Type the int numeric string (q - to quit):\n"); //prompt user to enter a number
+            fgets(int_string, BUFFER_SIZE, stdin); //putting each character into separate slots in the int_string array
+            int_string[strlen(int_string) - 1] = '\0'; // taking last slot in the array and putting the '0' character
+            int_number = atoi(int_string); //takes the int_string array and converts it to an int number
+            printf("Converted number is %d\n", int_number); //prints out the number
+        }while(strcmp(int_string, "q")); //as long as the user does not type in "q"
     printf("*** End of Converting Strings to int Demo ***\n\n");
 
 
