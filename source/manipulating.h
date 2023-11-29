3e92b9bd89d57eb38ca360e0f427d60ac9ddb185
smtg0007 <shaheersohail.ss@gmail.com>
2023-11-28 19:19:16 -0500

    Removed files from source and moved to V1

--- source/manipulating.h
+++
@@ -1,6 +0,0 @@
-#ifndef _MANIPULATING_H_ // This is the header guard. It prevents the header from being included more than once.
-#define _MANIPULATING_H_
-#include <stdio.h> 
-#include <string.h>
-void manipulating(void);
-#endif
