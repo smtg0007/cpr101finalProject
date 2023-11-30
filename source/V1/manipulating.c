48fa85346fbd237346ad92599761236754a5e32b
smtg0007 <shaheersohail.ss@gmail.com>
2023-11-30 02:51:19 -0500

    Fixed bug which didn't allow user to enter second string

--- source/V1/manipulating.c
+++ source/V1/manipulating.c
@@ -5,23 +5,22 @@
 void manipulating() {
 /* Version 1 */
 printf("*** Start of Concatenating Strings Demo ***\n");
-char string1[BUFFER_SIZE]; //initializing an array of characters which is 80 bytes long 
-char string2[BUFFER_SIZE]; //initializing an array of characters which is 80 bytes long
+char string1[BUFFER_SIZE]; //initializing an array of characters which is 80 bytes long
+char string2[BUFFER_SIZE];
+
 do{
-    printf("Type the 1st string (q - to quit):\n"); //prompt user to enter a string
+    printf("Type the 1st string (q - to quit):\n");
     fgets(string1, BUFFER_SIZE, stdin); //putting each character into separate slots in the string1 array
-    string1[strlen(string1) - 1] = '\0'; // taking last slot in the array and putting the '0' character
-    if ((strcmp(string1, "q")) == 0) { //if the user types in "q" then the program will quit
-        printf("Type the 2nd string:\n"); //prompt user to enter a string
+    string1[strlen(string1) -1] = '\0'; // taking last slot in the array and putting the '0' character
+    if ((strcmp(string1, "q") != 0)){
+        printf("Type the 2nd string:\n");
         fgets(string2, BUFFER_SIZE, stdin); //putting each character into separate slots in the string2 array
-        string2[strlen(string2) - 1] = '\0'; // taking last slot in the array and putting the '0' character
+        string2[strlen(string2) -1] = '\0'; // taking last slot in the array and putting the '0' character
         strcat(string1, string2); //concatenates string1 and string2
         printf("Concatenated string is \'%s\'\n", string1); //prints out the concatenated string
     }
-
-}while(strcmp(string1, "q")); //as long as the user does not type in "q"
+} while (strcmp(string1, "q") != 0); //as long as the user does not type in "q"
 printf("*** End of Concatenating Strings Demo ***\n\n");
-/* Version 2 */
 // >> insert here
 
 
@@ -29,4 +28,9 @@ printf("*** End of Concatenating Strings Demo ***\n\n");
 // >> insert here
 
 
+}
+
+int main()
+{
+    manipulating();
 }
