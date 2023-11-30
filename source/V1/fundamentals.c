ecf9c2d4865d1af6d59728549f57e17d9bb5c29b
smtg0007 <shaheersohail.ss@gmail.com>
2023-11-30 02:44:24 -0500

    Updated V1 of fundamentals by adding comments

--- source/V1/fundamentals.c
+++ source/V1/fundamentals.c
@@ -1,3 +1,5 @@
+//Created by: Shaheer Sohail
+/*The goal of this program is to find the position of a certain character in a user String input*/
 #define _CRT_SECURE_NO_WARNINGS
 #define BUFFER_SIZE 80
 #define NUM_INPUT_SIZE 10
@@ -5,25 +7,25 @@
 
 void fundamentals() {
 printf("*** start of Indexing Strings Demo ***\n");
-char buffer1[BUFFER_SIZE];
+char buffer1[BUFFER_SIZE]; //initializing an array of characters which is 80 bytes long
 char num_input[NUM_INPUT_SIZE];
 size_t position;
 do{
     printf("Type a string (q - to quit):\n");
-    fgets(buffer1, BUFFER_SIZE, stdin);
-    buffer1[strlen(buffer1) - 1] = '\0';
-    if(strcmp(buffer1, "q") != 0)
+    fgets(buffer1, BUFFER_SIZE, stdin); //putting each character into separate slots in the buffer1 array
+    buffer1[strlen(buffer1) - 1] = '\0'; // taking last slot in the array and putting the '0' character
+    if(strcmp(buffer1, "q") != 0) 
     {
         printf("Type the character Position within the string:\n");
-        fgets(num_input, NUM_INPUT_SIZE, stdin);
-        num_input[strlen(num_input) - 1] = '\0';
-        position = atoi (num_input);
-        if(position >= strlen(buffer1))
+        fgets(num_input, NUM_INPUT_SIZE, stdin); //putting each character into separate slots in the num_input array
+        num_input[strlen(num_input) - 1] = '\0'; // taking last slot in the array and putting the '0' character
+        position = atoi (num_input); //takes the num_input array and converts it to an int number
+        if(position >= strlen(buffer1)) 
         {
-            position = strlen( buffer1) - 1;
-            printf("Too big... Position reduced to max. available\n");
+            position = strlen( buffer1) - 1; //if the position is greater than the length of the string, then the position is set to the length of the string - 1
+            printf("Too big... Position reduced to max. available\n"); //prints out this message then finds the last character in the string
         }
-        printf("The character found at %d position is \'%c\'\n", (int)position, buffer1[position]);
+        printf("The character found at %d position is \'%c\'\n", (int)position, buffer1[position]); //prints out the number
     }
 }while(strcmp(buffer1, "q") != 0);
 
