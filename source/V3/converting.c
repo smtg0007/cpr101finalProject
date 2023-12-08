ede2b5f70db8e7505f7904b57c9180cb6e0bdb79
smtg0007 <shaheersohail.ss@gmail.com>
2023-12-07 22:27:46 -0500

    updated all versions

--- source/V3/converting.c
+++ source/V3/converting.c
@@ -11,11 +11,11 @@ printf("*** Start of converting Strings to long Demo ***\n");
 char longString[BUFFER_SIZE]; //initializing an array of characters which is 80 bytes long
 long longNumber; 
 do {
-    printf("Type the long numric string (q - to quit):\n"); //prompt user to enter a string
+    printf("Type the long numeric string (q - to quit):\n"); //prompt user to enter a string
     fgets(longString, BUFFER_SIZE, stdin); //putting each character into separate slots in the phrase array
     longString[strlen(longString) - 1] = '\0'; // taking last slot in the array and putting the '0' character
     if (strcmp(longString, "q") != 0) {
-        longNumber = atol(longString); //takes the phrase array and tokenizes it by commas
+        longNumber = atol(longString); 
         printf("Converted number is %ld\n", longNumber); //prints out the phrase and the number of the phrase
     }
 }while(strcmp(longString, "q") != 0);
